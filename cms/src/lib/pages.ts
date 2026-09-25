b0bf2a66746dbb12885bf8a186df40c1c73778aa
guptasomnath <131764381+guptasomnath@users.noreply.github.com>
2026-09-25 11:30:25 +0530

    created option to create pages

--- cms/src/lib/pages.ts
+++
@@ -1,5 +0,0 @@
-export const PAGES = [{ slug: 'home', name: 'Home' }] as const
-
-export type PageSlug = (typeof PAGES)[number]['slug']
-
-export const pageName = (slug: string) => PAGES.find((p) => p.slug === slug)?.name ?? slug
